0f031e7b1382a4f0041fc0be889b0a850bf413af
26fati <fatibeautiful1@gmail.com>
2023-09-21 13:32:11 +0100

    task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int min, tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		tmp = array[i];
+		array[i] = array[min];
+		array[min] = tmp;
+		print_array(array, size);
+	}		
+}
