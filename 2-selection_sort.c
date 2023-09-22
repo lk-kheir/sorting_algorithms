f1513fafb8883e88db8c9dfc0f60a9de02606469
26fati <101020848+26fati@users.noreply.github.com>
2023-09-22 22:35:27 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,7 +8,7 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
@@ -18,9 +18,12 @@ void selection_sort(int *array, size_t size)
 				min = j;
 			}
 		}
-		tmp = array[i];
-		array[i] = array[min];
-		array[min] = tmp;
-		print_array(array, size);
+		if (i != min)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
 	}		
 }
