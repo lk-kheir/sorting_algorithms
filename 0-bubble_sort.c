c8207d73149fca9aa052cb754a592d5ae851545a
26fati <fatibeautiful1@gmail.com>
2023-09-23 10:43:02 +0100

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,18 +20,19 @@ void bubble_sort(int *array, size_t size)
   for (i = 0; i < size; i++)
   {
 	  is_sorted = 0;
-    for (j = 0; j < size - 1; j++)
-    {
-      if (array[j + 1] < array[j] ){
-        temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-	is_sorted = 1;
-	print_array(array, size);
-      }
-    }
-    if (!is_sorted)
-              return;
+	  for (j = 0; j < size - 1; j++)
+	  {
+		  if (array[j + 1] < array[j] )
+		  {
+			  temp = array[j];
+			  array[j] = array[j + 1];
+			  array[j + 1] = temp;
+			  is_sorted = 1;
+			  print_array(array, size);
+		  }
+	  }
+	  if (!is_sorted)
+		  return;
   }
 
 }
