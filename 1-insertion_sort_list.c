30679635000de68c673b7ce6fcdc78a6372094ba
louzanizineddine <louzanizineddine@gmail.com>
2023-09-23 14:38:51 +0200

    betty for insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,11 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - insertion sort algorithm
+ * @list: list to be sorted
+ * Return: nothing
+ */
+
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *ptr, *tmp;
