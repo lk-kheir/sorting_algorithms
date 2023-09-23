b0dbb74b5bef11e7bbef55b32de2909ade32be2b
26fati <fatibeautiful1@gmail.com>
2023-09-23 10:48:47 +0100

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,28 +1,24 @@
 #include "sort.h"
 
-
+/**
+ * bubble_sort - bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: nothing
+*/
 void bubble_sort(int *array, size_t size)
 {
-  size_t i = 0, j = 0, temp;
-  size_t is_sorted;
+  size_t i, j, is_sorted;
+  int temp;
   if (array == NULL || size < 2)
 	  return;
- /* 
-  for (i = 0 ; i < size - 1 ; i++)
-  {
-    if(array[i] > array[i + 1]){
-      break;
-    } 
-  }
-  
-  if(i == size - 1) return;
-*/
+
   for (i = 0; i < size; i++)
   {
 	  is_sorted = 0;
 	  for (j = 0; j < size - 1; j++)
 	  {
-		  if (array[j + 1] < array[j] )
+		  if (array[j + 1] < array[j])
 		  {
 			  temp = array[j];
 			  array[j] = array[j + 1];
